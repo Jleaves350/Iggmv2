81bf36b9e5c527df005871c70f5d7ae21487f5f4
James350 <57271906+James350@users.noreply.github.com>
2023-09-19 15:03:25 +0100

    Complete

--- Iggmv2/Program.cs
+++ Iggmv2/Program.cs
@@ -1,14 +1,33 @@
 ﻿using OpenCvSharp;
 using System;
+using System.Diagnostics;
 public class program
 {
+    static Mat rotate_frame(Mat image, double angle)
+    {
+        var height = image.Height;
+        var width = image.Width;
+        Point centre = new Point(width / 2, height / 2);
+        Mat rot_mat = Cv2.GetRotationMatrix2D(centre, angle, 1.0);
+        Mat result = new Mat();
+        Cv2.WarpAffine(image, result, rot_mat, image.Size(), InterpolationFlags.Linear, BorderTypes.Constant, Scalar.All(0));
+        return result;
+    }
     static Mat normalise(Mat image)
     {
         double maxVal, minVal;
+        float val;
         Point minLoc, maxLoc;
         Cv2.MinMaxLoc(image, out minVal, out maxVal, out minLoc, out maxLoc);
-        Mat result = new Mat();
-        Cv2.Divide(image, maxVal, result);
+        Mat result = new Mat(image.Size(), MatType.CV_32FC1);
+        for (int row = 0; row < image.Rows; row++)
+        {
+            for(int cols=0; cols < image.Cols; cols++)
+            {
+                val = (float) (image.At<Byte>(row, cols) / maxVal);
+                result.At<float>(row,cols) = val;
+            }
+        }
         return result;
     }
     public static List<Point2f> processframe(Mat bef, Mat aft, double angle)//assume bef and aft have same dimension
@@ -18,75 +37,65 @@ public class program
         rows = bef.Rows;
         cols = bef.Cols;
         Mat diff = new Mat(rows,cols,MatType.CV_8UC1);
+        Mat diffthres = new Mat(rows, cols, MatType.CV_8UC1);
         //find the difference between bef and aft
         Cv2.Subtract(bef, aft, diff);
         //threshold diff
-        Cv2.Threshold(diff, diff, 120, 0, ThresholdTypes.Tozero);
-        //normalise diff
+        Cv2.Threshold(diff, diffthres, 120, 0, ThresholdTypes.Tozero);
+        //rotate image
+        diff = rotate_frame(diffthres, angle);
+        //normalise
         diff = normalise(diff);
-        //Create double sized image
-        Mat rot = new Mat(rows*2, cols*2, MatType.CV_8UC1);
-        //copy diff to rot with (0,0) in diff mapping to the centre of rot
-        for(int row = 0; row < rows; row++)
-        {
-            for(int col = 0; col < cols; col++)
-            {
-                rot.At<Byte>(row + 1079, col + 1919) = diff.At<Byte>(row, col);
-            }
-        }
-        //rotate rot around (1079,1919)
-        Point centre = new Point(1079,1919);
-        Mat rotation = Cv2.GetRotationMatrix2D(centre, angle, 1.0);
-        Mat post = new Mat();
-        Cv2.WarpAffine(rot, post, rotation,rot.Size());
-        //Grey gravity search
-        float sum1, sum2;
-        List<Point2f> points = new List<Point2f>();
-        for (int j = 0; j < rows*2; j++)
+        //IGGM
+        double sum1, sum2;
+        float pixval;
+        Mat testimg = new Mat(rows,cols,MatType.CV_8UC1);
+        List<Point2f> result = new List<Point2f>();
+        for (int j=0; j<cols; j++)
         {
             sum1 = 0;
             sum2 = 0;
-            for (int x = 0; x < cols*2; x++)
+            for(int x =0; x<rows; x++)
             {
-                sum1 += post.At<Byte>(j, x) * x;
-
-            }
-            for (int y = 0; y < cols - 1; y++)
-            {
-                sum2 += post.At<Byte>(j, y);
+             
+                pixval = (float)diff.At<float>(x, j);
+                sum1 += pixval*x;
+                sum2 += pixval;
             }
             if (sum2 == 0)
             {
                 continue;
             }
-            points.Add(new Point2f(j, sum1 / sum2));
+            result.Add(new Point2f((float)(sum1 / sum2),j));
+            testimg.At<Byte>((int)(sum1 / sum2),j)= (Byte)255;
         }
-        //Rotate points back to inital frame
-        List<Point2f> rotatedPoints = new List<Point2f>();
-        double ang = angle * (Math.PI / 180);
-        foreach(Point2f point in points)
-        {
-            double x = (point.X - 1079)*(Math.Cos(ang))- Math.Sin(ang)*(point.Y-1919);
-            double y = (point.X - 1079) * (Math.Sin(ang)) + Math.Cos(ang) * (point.Y - 1919);
-            rotatedPoints.Add(new Point2f());
-        }
-        //output points
-        foreach(Point2f point in points)
-        {
-            Console.WriteLine(point.ToString());
-            post.At<Byte>((int)point.X, (int)point.Y) = (Byte)255;
-        }
-        Cv2.ImShow("out", post);
+        //output line
+        testimg = rotate_frame(testimg, -angle);
+        Cv2.Add(diffthres, testimg, testimg);
+        Cv2.ImShow("Out img", testimg);
         Cv2.WaitKey(0);
-        //null condidtion
-        List<Point2f> bruh = new List<Point2f>();
-        return bruh;
+        //get height data
+        //return
+        return result;
     }
     static void Main(string[] args)
     {
         using var img1 = new Mat("C:\\Users\\james\\source\\repos\\Iggmv2\\Iggmv2\\pic1.jpg", ImreadModes.Grayscale);
         using var img2 = new Mat("C:\\Users\\james\\source\\repos\\Iggmv2\\Iggmv2\\pic2.jpg", ImreadModes.Grayscale);
         List<Point2f> bruh = new List<Point2f>();
+        Mat gamer = new Mat(img1.Size(), MatType.CV_8UC1);
+        double test;
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
         bruh = processframe(img1, img2, 45.0);
+        stopwatch.Stop();
+        long elap = stopwatch.ElapsedMilliseconds;
+        Console.WriteLine(elap);
+        foreach(Point2f point in bruh)
+        {
+            gamer.At<Byte>((int)point.X,(int)(point.Y-100))=(Byte)255;
+        }
+        Cv2.ImShow("Balls", gamer);
+        Cv2.WaitKey(0);
     }
 }
